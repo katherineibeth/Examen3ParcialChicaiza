61a95f6600dcbf47a8eb10a44bf5656c34c1a4ea
katherineibeth <kichicaiza@espe.edu.ec>
2024-03-11 19:31:14 -0500

    Creacion de Proyecto

---
+++ src/main/java/ec/espe/edu/tercerparcial/ventasChicaiza/VentasChicaizaApplication.java
@@ -0,0 +1,13 @@
+package ec.espe.edu.tercerparcial.ventasChicaiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasChicaizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasChicaizaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/espe/edu/tercerparcial/ventasChicaiza/VentasChicaizaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.espe.edu.tercerparcial.ventasChicaiza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasChicaizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
